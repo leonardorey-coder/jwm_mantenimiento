3cd5f3384ae1799df4bfc49d8bbd339176b7c289
leonardo <leonardo.cfjl@gmail.com>
2025-12-06 19:49:59 -0500

    feat: agregar configuración de base de datos local y producción para PostgreSQL

--- db/config.js
+++ db/config.js
@@ -9,8 +9,8 @@ let dbConfig = {
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '5432'),
     database: process.env.DB_NAME || 'jwmantto',
-    user: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || 'postgres',
+    user: process.env.DB_USER || 'leonardocruz',
+    password: process.env.DB_PASSWORD || '',
     
     // Configuración del pool de conexiones
     max: parseInt(process.env.DB_POOL_MAX || '20'), // Máximo de conexiones en el pool
